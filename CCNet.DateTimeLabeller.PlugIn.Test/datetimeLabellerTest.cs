ed41c0967fc76cdb0783a222389cf4568bc49989
Toby Qin <cicaday@gmail.com>
2015-04-27 13:20:11 +0800

    Add project.

---
+++ CCNet.DateTimeLabeller.PlugIn.Test/datetimeLabellerTest.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ThoughtWorks.CruiseControl.Core;
+
+namespace ccnet.datetimelabeller.plugin.Test
+{
+    /// <summary>
+    ///This is a test class for tfsRevisionLabellerTest and is intended
+    ///to contain all tfsRevisionLabellerTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class datetimeLabellerTest
+    {
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+
+        //
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+
+        #endregion Additional test attributes
+
+        /// <summary>
+        ///A test for Generate
+        ///</summary>
+        [TestMethod()]
+        public void GenerateVersion_With_Provided()
+        {
+            DatetimeLabeller target = new DatetimeLabeller(); // TODO: Initialize to an appropriate value
+            IIntegrationResult integrationResult = new IntegrationResult(); // TODO: Initialize to an appropriate value
+
+            target.Major = 3;
+            target.Minor = 1;
+            target.Build = "0";
+            target.Prefix = "Prod-";
+            target.DateTimeFormat = "MMdd";
+            string expected = "Prod-3.1.0.1";
+            string actual;
+            actual = target.Generate(integrationResult);
+            Assert.AreEqual(expected, actual);
+            TestContext.WriteLine("Actual Build Numberb: " + actual);
+        }
+
+        /// <summary>
+        ///A test for Generate
+        ///</summary>
+        [TestMethod()]
+        public void GenerateVersion_With_Year()
+        {
+            DatetimeLabeller target = new DatetimeLabeller(); // TODO: Initialize to an appropriate value
+            IIntegrationResult integrationResult = new IntegrationResult(); // TODO: Initialize to an appropriate value
+
+            target.Major = 3;
+            target.Minor = 1;
+            target.DateTimeFormat = "yyyy";
+            string expected = "3.1.2015.1";
+            string actual;
+            actual = target.Generate(integrationResult);
+            Assert.AreEqual(expected, actual);
+            TestContext.WriteLine("Actual Build Numberb: " + actual);
+        }
+
+        /// <summary>
+        ///A test for Generate
+        ///</summary>
+        [TestMethod()]
+        public void GenerateVersion_With_MMdd()
+        {
+            DatetimeLabeller target = new DatetimeLabeller(); // TODO: Initialize to an appropriate value
+            IIntegrationResult integrationResult = new IntegrationResult(); // TODO: Initialize to an appropriate value
+
+            target.DateTimeFormat = "MMdd";
+            string expected = "1.0.0427.1";
+            string actual;
+            actual = target.Generate(integrationResult);
+            Assert.AreEqual(expected, actual);
+            TestContext.WriteLine("Actual Build Numberb: " + actual);
+        }
+    }
+}
---
+++ CCNet.DateTimeLabeller.PlugIn/datetimeLabeller.cs
@@ -0,0 +1,134 @@
+﻿using Exortech.NetReflector;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using ThoughtWorks.CruiseControl.Core;
+using ThoughtWorks.CruiseControl.Core.Config;
+using ThoughtWorks.CruiseControl.Core.Util;
+using ThoughtWorks.CruiseControl.Remote;
+
+namespace ccnet.datetimelabeller.plugin
+{
+    [ReflectorType("DatetimeLabeller")]
+    public class DatetimeLabeller : ITask, ILabeller
+    {
+        public DatetimeLabeller()
+        {
+        }
+
+        #region ITask Members
+
+        public void Run(IIntegrationResult result)
+        {
+            result.Label = this.Generate(result);
+        }
+
+        #endregion ITask Members
+
+        #region ILabeller Members
+
+        public string Generate(IIntegrationResult integrationResult)
+        {
+            string version = "0.0.0.1";
+            if (string.IsNullOrEmpty(_build))
+            {
+                _build = GetBuild(this.DateTimeFormat);
+            }
+            var _rebuild = GetRebuildNumber(_build, integrationResult.LastSuccessfulIntegrationLabel);
+            version = string.Format("{0}{1}.{2}.{3}.{4}", Prefix, Major, Minor, Build, _rebuild);
+            return version;
+        }
+
+        private int GetRebuildNumber(string currentBuild, string lastLabel)
+        {
+            var p = @"([\d]+)\.([\d]+)\.([\d]+)\.([\d+])";
+            var v = Regex.Match(lastLabel, p).Groups[3].Value;
+            var r = Regex.Match(lastLabel, p).Groups[4].Value;
+
+            int rebuild = 0;
+            Int32.TryParse(r, out rebuild);
+
+            if (v == currentBuild)
+            {
+                rebuild++;
+            }
+            else
+            {
+                rebuild = 1;
+            }
+            return rebuild;
+        }
+
+        private string GetBuild(string datetimeFormat)
+        {
+            return DateTime.Now.ToString(datetimeFormat);
+        }
+
+        #endregion ILabeller Members
+
+        /// <summary>
+        /// Gets or sets the major version.
+        /// </summary>
+        /// <value>The major version number.</value>
+        private int _major = 1;
+
+        [ReflectorProperty("major", Required = false)]
+        public int Major
+        {
+            get { return _major; }
+            set { _major = value; }
+        }
+
+        /// <summary>
+        /// Gets or sets the minor version.
+        /// </summary>
+        /// <value>The minor version number.</value>
+        private int _minor = 0;
+
+        [ReflectorProperty("minor", Required = false)]
+        public int Minor
+        {
+            get { return _minor; }
+            set { _minor = value; }
+        }
+
+        /// <summary>
+        /// Gets or sets the build number.
+        /// </summary>
+        /// <value>The build number.</value>
+        private string _build = "";
+
+        [ReflectorProperty("build", Required = false)]
+        public string Build
+        {
+            get { return _build; }
+            set { _build = value; }
+        }
+
+        /// <summary>
+        /// Gets or sets the datetime format in build.
+        /// </summary>
+        /// <value>The build format.</value>
+        private string _format = "yyyyMMdd";
+
+        [ReflectorProperty("datetimeFormat", Required = false)]
+        public string DateTimeFormat
+        {
+            get { return _format; }
+            set { _format = value; }
+        }
+
+        /// <summary>
+        /// Prefix for the build label.
+        /// </summary>
+        [ReflectorProperty("prefix", Required = false)]
+        public string Prefix { get; set; }
+    }
+}
